72a31026078296d67429dd903bcecdf84aceec12
sherlockgames <harshuajmani@gmail.com>
2024-07-17 18:44:34 +0530

    Used scene manger and Invoke. Two levels at the moment over and under

--- ProjectBoost/Assets/Scripts/CollisionHandler.cs
+++ ProjectBoost/Assets/Scripts/CollisionHandler.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] float levelLoadDelay = 1f;
     void OnCollisionEnter(Collision other)
     {
         switch (other.gameObject.tag)
@@ -9,16 +11,46 @@ public class CollisionHandler : MonoBehaviour
             case "Friendly":
                 Debug.Log("Friendly");
                 break;
-            case "Fuel":
-                Debug.Log("Fuel");
-                break;
             case "Finish":
                 Debug.Log("Finish");
+                StartSucessSquence();
                 break;
             default:
                 Debug.Log("unFriendly");
+                StartCrashSequence();
                 break; 
         }
     }
-    
+
+    void StartCrashSequence()
+    {
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", levelLoadDelay);
+    }
+
+    void StartSucessSquence()
+    {
+        GetComponent<Movement>().enabled = false;
+        Invoke("LoadNextLevel", levelLoadDelay);
+    }
+    //To load the scene we are currently on when the rocket crashes to an unfriendly object 
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
 }
